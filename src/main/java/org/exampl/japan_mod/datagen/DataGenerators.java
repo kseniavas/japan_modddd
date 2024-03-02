1dc601a0617908ea432c6284dc7fcc8303268f42
kseniavas <114919991+kseniavas@users.noreply.github.com>
2024-03-02 16:04:26 +0300

    Initial commit

---
+++ src/main/java/org/exampl/japan_mod/datagen/DataGenerators.java
@@ -0,0 +1,26 @@
+package org.exampl.japan_mod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.exampl.japan_mod.Japan_mod;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = Japan_mod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
+    }
+}
---
+++ src/main/java/org/exampl/japan_mod/worldgen/tree/custom/MapleFoliagePlacer.java
@@ -0,0 +1,48 @@
+package org.exampl.japan_mod.worldgen.tree.custom;
+
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
+import org.exampl.japan_mod.worldgen.tree.ModFoliagePlacers;
+
+public class MapleFoliagePlacer extends FoliagePlacer {
+    public static final Codec<MapleFoliagePlacer> CODEC = RecordCodecBuilder.create(mapleFoliagePlacerInstance
+            -> foliagePlacerParts(mapleFoliagePlacerInstance).and(Codec.intRange(0, 16).fieldOf("height")
+            .forGetter(fp -> fp.height)).apply(mapleFoliagePlacerInstance, MapleFoliagePlacer::new));
+    private final int height;
+
+    public MapleFoliagePlacer(IntProvider pRadius, IntProvider pOffset, int height) {
+        super(pRadius, pOffset);
+        this.height = height;
+    }
+
+    @Override
+    protected FoliagePlacerType<?> type() {
+        return ModFoliagePlacers.MAPLE_FOLIAGE_PLACER.get();
+    }
+
+    @Override
+    protected void createFoliage(LevelSimulatedReader pLevel, FoliageSetter pBlockSetter, RandomSource pRandom, TreeConfiguration pConfig,
+                                 int pMaxFreeTreeHeight, FoliageAttachment pAttachment, int pFoliageHeight, int pFoliageRadius, int pOffset) {
+
+        this.placeLeavesRow(pLevel, pBlockSetter, pRandom, pConfig, pAttachment.pos().above(0), 2, 2, pAttachment.doubleTrunk());
+        this.placeLeavesRow(pLevel, pBlockSetter, pRandom, pConfig, pAttachment.pos().above(1), 2, 2, pAttachment.doubleTrunk());
+        this.placeLeavesRow(pLevel, pBlockSetter, pRandom, pConfig, pAttachment.pos().above(2), 2, 2, pAttachment.doubleTrunk());
+    }
+
+    @Override
+    public int foliageHeight(RandomSource pRandom, int pHeight, TreeConfiguration pConfig) {
+        return this.height;
+    }
+
+    @Override
+    protected boolean shouldSkipLocation(RandomSource pRandom, int pLocalX, int pLocalY, int pLocalZ, int pRange, boolean pLarge) {
+        return false;
+    }
+}
