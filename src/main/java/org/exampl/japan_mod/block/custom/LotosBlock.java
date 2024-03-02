11fc4d96fc6544c168baba718b6c082acf6647ca
SimobiSirOP <simobisirop@gmail.com>
2024-03-02 17:45:14 +0300

    Cansurvive для лотосика

--- src/main/java/org/exampl/japan_mod/block/custom/LotosBlock.java
+++ src/main/java/org/exampl/japan_mod/block/custom/LotosBlock.java
@@ -2,6 +2,7 @@ package org.exampl.japan_mod.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.GrassBlock;
 import net.minecraft.world.level.block.IceBlock;
@@ -21,5 +22,9 @@ public class LotosBlock extends WaterlilyBlock {
         return (fluidstate.getType() == Fluids.WATER || pState.getBlock() instanceof IceBlock) && fluidstate1.getType() == Fluids.EMPTY&&pState.is(Blocks.WATER);
 
     }
-
+    @Override
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        BlockPos blockpos = pPos.below();
+        return this.mayPlaceOn(pLevel.getBlockState(blockpos), pLevel, blockpos);
+    }
 }
