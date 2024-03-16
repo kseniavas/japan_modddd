9b30f69fbe0c1414770b7821365340d2c21e5f92
kseniavas <114919991+kseniavas@users.noreply.github.com>
2024-03-16 19:06:55 +0300

    Initial commit

--- src/main/java/org/exampl/japan_mod/block/custom/ModFlammableRotatedPillarBlock.java
+++ src/main/java/org/exampl/japan_mod/block/custom/ModFlammableRotatedPillarBlock.java
@@ -1,33 +1,49 @@
 package org.exampl.japan_mod.block.custom;
+import org.exampl.japan_mod.block.ModBlocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ToolAction;
-
-import javax.annotation.Nullable;
+import org.checkerframework.checker.units.qual.A;
+import org.jetbrains.annotations.Nullable;
 
 public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
-
     public ModFlammableRotatedPillarBlock(Properties pProperties) {
         super(pProperties);
     }
+
     @Override
-    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction){
+    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
         return true;
     }
 
     @Override
-    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction){
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
         return 5;
     }
 
     @Override
-    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction){
-        return 5;}
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 5;
+    }
 
+    @Override
+    public @Nullable BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction, boolean simulate) {
+        if(context.getItemInHand().getItem() instanceof AxeItem) {
+            if(state.is(ModBlocks.MAPLE_LOG.get())) {
+                return ModBlocks.STRIPPED_MAPLE_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
 
+            if(state.is(ModBlocks.MAPLE_WOOD.get())) {
+                return ModBlocks.STRIPPED_MAPLE_WOOD.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
+        }
+
+        return super.getToolModifiedState(state, context, toolAction, simulate);
+    }
 }
